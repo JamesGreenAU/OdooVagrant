98556d74022ba462d680b57898121dda4e0c4c7b
James Green <james.green@technocrat.com.au>
2015-10-15 16:13:17 +1100

    Add basic add-in scaffolding

---
+++ odoo_modules/basic_addin/__openerp__.py
@@ -0,0 +1,16 @@
+{
+  'name': 'Sample Addin',
+  'description': 'Provides a sample add-in.',
+  'author': 'JamesGreenAU',
+  'depends': ['base'],
+  'application': True,
+  'summary': "Provides a sample add-in.",
+  'website': "https://deepdark.net",
+  'license': "Other proprietary", # Valid types are here: http://useopenerp.com/v8/model/ir-module-module
+  'category': 'Specific Industry Applications', # Valid categories https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+  'version': '0.0.3',
+  'installable': 'true',
+  'auto_install': 'false' ,
+  'data': [ ],
+  'demo': [ ]
+}
